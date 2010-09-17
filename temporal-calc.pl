0a8271700066f663fff5857bcdf438aa37a4abd4
Michael Moon <triffid.hunter@gmail.com>
2010-09-18 09:35:42 +1000

    change counters to decrement, explore some optimisations

--- temporal-calc.pl
+++ temporal-calc.pl
@@ -5,15 +5,15 @@ use Data::Dumper;
 
 my $f								= 800; # mm/min
 
-my $x								= 50;	# mm
-my $y								= 30;	# mm
+my $x								= 12;	# mm
+my $y								= 8;	# mm
 my $z								= 0.4;	#mm
-my $e								= 18;		#mm
+my $e								= 6;		#mm
 
-my $x_steps_per_mm	= 4;
-my $y_steps_per_mm	= 4;
-my $z_steps_per_mm	= 350;
-my $e_steps_per_mm	= 23;
+my $x_steps_per_mm	= 2;
+my $y_steps_per_mm	= 2;
+my $z_steps_per_mm	= 160;
+my $e_steps_per_mm	= 3;
 
 my $f_cpu						= 16000;
 
@@ -30,30 +30,29 @@ printf "X:%gmm (%i steps)\tY:%gmm (%i steps)\tZ:%gmm (%i steps)\tE:%gmm (%i step
 
 my $ct = 0;
 
-my $xc = 0;
-my $yc = 0;
-my $zc = 0;
-my $ec = 0;
 my $xd = ($x_steps > 0)?int($time / $x_steps):$time;
 my $yd = ($y_steps > 0)?int($time / $y_steps):$time;
 my $zd = ($z_steps > 0)?int($time / $z_steps):$time;
 my $ed = ($e_steps > 0)?int($time / $e_steps):$time;
+my $xc = $xd;
+my $yc = $yd;
+my $zc = $zd;
+my $ec = $ed;
 
 my $td;
 
-# my $td = $time;
-# if ((($xd - $xc) < $td) && $x_steps) {
-	$td = $xd - $xc;
+my $td;
+# if ($xc < $td) {
+	$td = $xc;
 # }
-if ((($yd - $yc) < $td) && $y_steps) {
-	$td = $yd - $yc;
+if ($yc < $td) {
+	$td = $yc;
 }
-
-if ((($zd - $zc) < $td) && $z_steps) {
-	$td = $zd - $zc;
+if ($zc < $td) {
+	$td = $zc;
 }
-if ((($ed - $ec) < $td) && $e_steps) {
-	$td = $ed - $ec;
+if ($ec < $td) {
+	$td = $ec;
 }
 
 my $last_x_step_time = 0;
@@ -65,33 +64,33 @@ while (($x_steps + $y_steps + $z_steps + $e_steps) > 0) {
 	my $st = "";
 
 	printf "\t";
-	if ($xc >= $xd) {
+	if ($xc <= 0) {
 		printf "[X Speed: %5.2fmm/sec] ", 1 / (($ct - $last_x_step_time) / $f_cpu) / $x_steps_per_mm;
 		$last_x_step_time = $ct;
-		$xc -= $xd;
+		$xc += $xd;
 		$x_steps--;
 		$st .= "X ";
 	}
-	if ($yc >= $yd) {
+	if ($yc <= 0) {
 		printf "[Y Speed: %5.2fmm/sec] ", 1 / (($ct - $last_y_step_time) / $f_cpu) / $y_steps_per_mm;
 		$last_y_step_time = $ct;
-		$yc -= $yd;
+		$yc += $yd;
 		$y_steps--;
 		$st .= "Y ";
 	}
-	if ($zc >= $zd) {
+	if ($zc <= 0) {
 		printf "[Z Speed: %5.2fmm/sec] ", 1 / (($ct - $last_z_step_time) / $f_cpu) / $z_steps_per_mm;
 		$last_z_step_time = $ct;
-		$zc -= $zd;
+		$zc += $zd;
 		$z_steps--;
 		$st .= "Z ";
 	}
-	if ($ec >= $ed) {
-		die Dumper ['$e_steps_per_mm' => $e_steps_per_mm, 'ct' => $ct, 'last_e_step_time' => $last_e_step_time]
-			if ((($ct - $last_e_step_time) / $f_cpu) / $e_steps_per_mm) == 0;
+	if ($ec <= 0) {
+# 		die Dumper ['$e_steps_per_mm' => $e_steps_per_mm, 'ct' => $ct, 'last_e_step_time' => $last_e_step_time]
+# 			if ((($ct - $last_e_step_time) / $f_cpu) / $e_steps_per_mm) == 0;
 		printf "[E Speed: %5.2fmm/sec] ", 1 / (($ct - $last_e_step_time) / $f_cpu) / $e_steps_per_mm;
 		$last_e_step_time = $ct;
-		$ec -= $ed;
+		$ec += $ed;
 		$e_steps--;
 		$st .= "E ";
 	}
@@ -115,32 +114,32 @@ while (($x_steps + $y_steps + $z_steps + $e_steps) > 0) {
 	my $e_ideal_pos = $e - ($e * $ct / $time);
 	my $e_error = $e_ideal_pos - $e_actual_pos;
 
-	printf stderr "%5i %s %s %s %s %s %s %s %s\n", $ct, (($xc == 0)?sprintf("%5.2f", $x_actual_pos):"no-st"), sprintf("%+5.3f", $x_error), (($yc == 0)?sprintf("%5.2f", $y_actual_pos):"no-st"), sprintf("%+5.3f", $y_error), (($zc == 0)?sprintf("%5.2f", $z_actual_pos):"no-st"), sprintf("%+5.3f", $z_error), (($ec == 0)?sprintf("%5.2f", $e_actual_pos):"no-st"), sprintf("%+5.3f", $e_error);
+	printf stderr "%g %s %s %s %s %s %s %s %s\n", $ct / $f_cpu, (($st =~ /X/)?sprintf("%g", $x_actual_pos):"no-st"), sprintf("%+g", $x_error), (($st =~ /Y/)?sprintf("%g", $y_actual_pos):"no-st"), sprintf("%+g", $y_error), (($st =~ /Z/)?sprintf("%g", $z_actual_pos):"no-st"), sprintf("%+g", $z_error), (($st =~ /E/)?sprintf("%g", $e_actual_pos):"no-st"), sprintf("%+g", $e_error);
 
-	$td = $time;
-	if (($xd - $xc) < $td) {
-		$td = $xd - $xc;
+# 	$td = $time;
+# 	if ($xc < $td) {
+		$td = $xc;
+# 	}
+	if ($yc < $td) {
+		$td = $yc;
 	}
-	if (($yd - $yc) < $td) {
-		$td = $yd - $yc;
+	if ($zc < $td) {
+		$td = $zc;
 	}
-	if (($zd - $zc) < $td) {
-		$td = $zd - $zc;
-	}
-	if (($ed - $ec) < $td) {
-		$td = $ed - $ec;
+	if ($ec < $td) {
+		$td = $ec;
 	}
 
-	$xc += $td
+	$xc -= $td
 		if $x_steps;
-	$yc += $td
+	$yc -= $td
 		if $y_steps;
-	$zc += $td
+	$zc -= $td
 		if $z_steps;
-	$ec += $td
+	$ec -= $td
 		if $e_steps;
 
-	printf "\t%5i (%5.2fs) + %3i = ", $ct, $ct / $f_cpu, $td;
+	printf "\t%5i (%5.3fs) + %3i = ", $ct, $ct / $f_cpu, $td;
 	$ct += $td;
-	printf "%5i (%5.2fs)\n", $ct, $ct / $f_cpu;
+	printf "%5i (%5.3fs)\n", $ct, $ct / $f_cpu;
 }
--- temporal-calc.plot
+++ temporal-calc.plot
@@ -1,7 +1,13 @@
-set terminal png
+set terminal png size 1024,768
 set output "temporal.png"
+# set terminal x11 persist
 set xrange [*:*]
-plot	"temporal_data" using ($1):($2):($3 * 1) title "X" with yerrorlines,\
-	"temporal_data" using ($1):($4):($5 * 1) title "Y" with yerrorlines,\
-	"temporal_data" using ($1):($6):($7 * 1) title "Z" with yerrorlines,\
-	"temporal_data" using ($1):($8):($9 * 1) title "E" with yerrorlines
+set xlabel "seconds"
+set yrange [0:13]
+set ylabel "millimeters from target"
+set y2range [0:1300]
+set y2label "* 100 micrometers from target"
+plot	"temporal_data" using ($1):($2):($3 >= 0?$2:$2 + $3 * 50):($3 >= 0?$2 - $3 * 50:$2) title "X" with yerrorbars pt 2 lw 0 lc rgb "red",\
+			"temporal_data" using ($1):($4):($5 >= 0?$4:$4 + $5 * 50):($5 >= 0?$4 - $5 * 50:$4) title "Y" with yerrorbars pt 2 lw 0 lc rgb "green",\
+			"temporal_data" using ($1):($6 * 1000):($7 >= 0?$6 * 1000:$6 * 1000 + $7 * 50):($7 >= 0?$6 * 1000 - $7 * 50:$6 * 1000) title "Z" with yerrorbars pt 2 lw 0 lc rgb "cyan" axes x1y2,\
+			"temporal_data" using ($1):($8):($9 >= 0?$8:$8 + $9 * 50):($9 >= 0?$8 - $9 * 50:$8) title "E" with yerrorbars pt 2 lw 0 lc rgb "violet"
